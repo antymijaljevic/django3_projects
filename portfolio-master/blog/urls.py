2446ea65ccec3307854a87d251b0bcb3bffa7233
antymijaljevic <antymijaljevic@gmail.com>
2020-11-29 20:09:03 +0000

    new

--- portfolio-master/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', views.all_blogs, name ='all_blogs'),
-    path('<int:blog_id>/', views.detail, name ='detail'),
-]
--- portfolio-master/portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-app_name = 'portfolio'
-
-urlpatterns = [
-    path('projects/', views.projects, name ='projects'),
-    path('about/', views.about, name ='about'),
-]
