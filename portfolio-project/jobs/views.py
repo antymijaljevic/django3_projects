85b6e047e06c66c595f5d1668ee7b4c65a248b5f
antymijaljevic <antymijaljevic@gmail.com>
2020-11-30 00:50:32 +0000

    new

---
+++ portfolio-project/jobs/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Job
+
+
+
+def home(request):
+    jobs = Job.objects.all()
+    return render(request, 'home.html', {
+        'jobs':jobs,
+        })
+
+
+def detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk = job_id) #primary key
+    return render(request, 'detail.html', {
+        'job':job_detail,
+    })
